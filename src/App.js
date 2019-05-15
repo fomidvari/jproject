3d191bde2287c2ec8b0a4379ffe428c7d9981d2f
farnaz <omidvary@gmail.com>
2019-05-15 17:37:44 +0430

    change learn react to learn jenkinz :)

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn Jenkins</a>
       </header>
     </div>
   );
